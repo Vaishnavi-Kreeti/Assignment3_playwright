217742aa823ce66de7e6263dda303bb55c63ea39
Vaishnavi-Kreeti <vaishnavi.shaw@kawach.ai>
2026-08-25 17:19:28 +0530

    hamburger

--- tests/functional/hamburger.spec.js
+++ tests/functional/hamburger.spec.js
@@ -10,17 +10,39 @@ test("hamburger",async function ({page}){
     const basepage=new BasePage(page);
     const loginpage=new LoginPage(page);
     const hamburger=new Hamburger(page);
+    const cart=new Cart(page);
 
     await page.goto(basepage.url)
     await loginpage.login("standard_user", "tta_secret")
     await expect(page).toHaveURL(/inventory/)
-   //opening hamburger flyout
-   await expect(hamburger.flyout).not.toHaveClass(/is-open/)
+    //opening hamburger flyout
+    await expect(hamburger.flyout).not.toHaveClass(/is-open/)
     await basepage.hamburger.click()
-    await expect(hamburger.flyout).toBeVisible()
+    await expect(hamburger.flyout).toHaveClass(/is-open/)
+   
     
-    //
+    //ist option check
     await hamburger.items.click()
-    await expect(hamburger.reset).toBeVisible()
-    await expect(hamburger.flyout).toBeVisible()
+    await expect(hamburger.flyout).not.toHaveClass(/is-open/)
+    await expect(page).toHaveURL(/inventory/)
+
+    //2nd option check
+    await basepage.hamburger.click()
+    await hamburger.about.click()
+    await expect(page).toHaveURL(/thetestingacademy/)
+
+    //4th option check
+    await page.goBack()
+    await basepage.hamburger.click()
+    await hamburger.reset.click()
+    const c=await cart.remove_btn.count()
+    await expect(c==0).toBeTruthy()
+    await expect(basepage.cart_count).not.toBeVisible()
+    await expect(hamburger.flyout).not.toHaveClass(/is-open/)
+
+    //3rd option check
+    await basepage.hamburger.click()
+    await hamburger.logout.click()
+    await expect(page).not.toHaveURL(/inventory/)
+    await expect(page).toHaveTitle("TTACart - Login");
 });
